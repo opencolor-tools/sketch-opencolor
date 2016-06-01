7ed5582bfa9e3cd7d48e62f0c4a82471e34efb13
Michael Schieben <babe@rockitbaby.de>
2016-06-01 12:53:01 +0200

    bump version

--- src/plugin.js
+++ src/plugin.js
@@ -5,7 +5,7 @@ export const HKSketchFusionExtension = {
   description: "",
   author: "Jan Krutisch, Florian Munz, Michael Schieben",
   authorEmail: "info@opencolor.tools",
-  version: "1.6.1",
+  version: "1.6.2",
   identifier: "tools.opencolor.sketch.opencolor",
   menu: {
     "isRoot": false,
