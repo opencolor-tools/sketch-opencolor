05f6e1924d80506405d98f3c113b65ce5f9597c7
Christophe Stoll <ch@precious-forever.com>
2016-07-08 22:19:10 +0200

    fixed typo

--- src/commands/settings.js
+++ src/commands/settings.js
@@ -2,7 +2,7 @@ import { selectOcoFile, getDefaultPalette, setDefaultPalette } from '../utils/oc
 import { getName } from '../utils/oco'
 
 export default function settings (context) {
-  var result = selectOcoFile('The default palette is used to identity colors in any sketch document (when artboards are not linked to a palette)', 'Set Default', getDefaultPalette(), true)
+  var result = selectOcoFile('The default palette is used to identify colors in any sketch document (when artboards are not linked to a palette)', 'Set Default', getDefaultPalette(), true)
   if (!result) { return }
   setDefaultPalette(result)
 
